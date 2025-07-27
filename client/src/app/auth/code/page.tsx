d5229d11020da2ecf60e900908397ff84343b6e9
iaroslav_shavrov <iaroslav_shavrov@practice.relex.ru>
2025-07-27 14:31:53 +0300

    auth. Форма ввода подтверждающего кода

---
+++ client/src/app/auth/code/page.tsx
@@ -0,0 +1,11 @@
+import ConfirmationCodeForm from "@/features/auth/components/ConfirmationCodeForm/ConfirmationCodeForm";
+
+
+export default function ConfirmPage() {
+    return (
+        <div>
+            <ConfirmationCodeForm />
+        </div>
+    );
+}
+
